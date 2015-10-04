1b82bf7c8b8d46f6fd591a0633d3e3d12c082561
Christopher Cowan <chrisjoelcowan@gmail.com>
2015-10-04 09:36:05 -0400

    More Work

--- Emme/Models/Document.cs
+++ Emme/Models/Document.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace Emme.Models
 {
     public class Document
     {
+        private GapBuffer<char> buffer = new GapBuffer<char>();
+
         public void Insert(Position position, char value)
         {
+            buffer.InsertAt(position.Column, value);
+        }
 
+        public void Write(TextWriter writer)
+        {
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                writer.Write(buffer[i]);
+            }
         }
     }
 }
